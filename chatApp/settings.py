850f4009e24813c6ffab38247788527624a67bd7
matrix-bro <razmhz11@gmail.com>
2023-02-25 15:01:31 +0545

    Integrated the Channels Library

--- chatApp/settings.py
+++ chatApp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -70,6 +71,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'chatApp.wsgi.application'
+# Daphne
+ASGI_APPLICATION = "chatApp.asgi.application"
 
 
 # Database
